e39bbf798ddd3fd0ae75910419525b71bc62cb36
ChhotuBhaluu <153289980+ChhotuBhaluu@users.noreply.github.com>
2023-12-08 21:04:17 +0530

    Update script.js

--- src/css/js/script.js
+++ src/css/js/script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on 1st!";
+  question.innerHTML = "Yay, see you on 1st Jan!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
